bd2575277baedd5ddf937b0f08410c72dbf7e79b
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-07-21 13:21:29 -0300

    allow-empty

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from imageai.Detection import ObjectDetection
+
+def detectar_objetos_na_imagem(input_image, output_image, model_path):
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()  # Usar o modelo YOLOv3
+    detector.setModelPath(model_path)  # Caminho para o modelo pré-treinado YOLOv3
+    detector.loadModel()
