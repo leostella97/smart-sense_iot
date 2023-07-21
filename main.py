5e5ff76d3c5d70cfca8fcec2b21f80e15aed48df
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-07-21 13:22:01 -0300

    allow-empty

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ def detectar_objetos_na_imagem(input_image, output_image, model_path):
     detector.setModelTypeAsYOLOv3()  # Usar o modelo YOLOv3
     detector.setModelPath(model_path)  # Caminho para o modelo pré-treinado YOLOv3
     detector.loadModel()
+
+    # Detecção de objetos na imagem de entrada
+    detections = detector.detectObjectsFromImage(input_image, output_image_path=output_image, minimum_percentage_probability=30)
+
+    
